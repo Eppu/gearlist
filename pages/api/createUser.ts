900465fb286d9eb56d16f81f9b705739c78a2079
Eetu Eskelinen <eetuesk@gmail.com>
2022-08-01 18:36:50 +0300

    Refactor prisma client creation and session checking to separate utility functions

--- pages/api/createUser.ts
+++ pages/api/createUser.ts
@@ -1,14 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import { prisma } from '../../lib/prisma';
 
-// Create a new instance of PrismaClient
-const prisma = new PrismaClient();
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   if (req.method === 'POST') {
     // Create a user in the DB and return the user
     const { name, email } = req.body;
--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,48 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
-import { unstable_getServerSession } from 'next-auth';
-import { authOptions } from './auth/[...nextauth]';
+import checkSession from '../../lib/checkSession';
+import { prisma } from '../../lib/prisma';
 
-// Create a new instance of PrismaClient
-const prisma = new PrismaClient();
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  // Get the session from the Next.js API
-  const session = await unstable_getServerSession(req, res, authOptions);
-  // Make sure the session is valid
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const session = await checkSession(req, res);
+  // There's probably a smarter way to do this but I can't come up with one right now.
   if (!session) {
     res.status(401).json({ message: 'Invalid session' });
-    return;
-  }
-  // Get the user from the session
-  const user = session.user;
-  // Make sure the user is valid
-  if (!user) {
-    res.status(401).json({ message: 'Invalid user' });
-    return;
-  }
-  console.log(user);
-  // Get the user's ID from the session
-  const userId = user.id;
-  // Make sure the user's ID is valid
-  if (!userId) {
-    res.status(401).json({ message: 'Invalid user ID' });
-    return;
-  }
-
-  // Make sure the user ID matches the incoming request
-  if (userId !== req.query.userId) {
-    res.status(401).json({ message: 'This user does not have access.' });
     return;
   }
 
   // Get all items with the request body user id from the db
   if (req.method === 'GET') {
-    const { id } = req.query;
+    const { id } = session.user;
     await prisma.item
       .findMany({
         where: {
