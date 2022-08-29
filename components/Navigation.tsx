2d1d2cfbfc9c5f75a664e2899b2b245e93c4f2c7
Eetu Eskelinen <eetuesk@gmail.com>
2022-08-29 22:44:36 +0300

    Improve navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,22 +1,24 @@
-import { Container, Text, Navbar, Button, Link, Card, Avatar, Dropdown } from '@nextui-org/react';
-import { useSession, getSession } from 'next-auth/react';
+import { Text, Navbar, Button, Link, Card, Avatar, Dropdown, Loading } from '@nextui-org/react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { Key } from 'react';
+
+function handleDropdownAction(action: Key) {
+  if (action === 'signOut') {
+    signOut();
+    return;
+  }
+  console.log(action);
+}
 
 export const Navigation = ({}) => {
   const { data: session, status } = useSession();
-  const collapseItems = [
-    'Profile',
-    'Dashboard',
-    'Activity',
-    'Analytics',
-    'System',
-    'Deployments',
-    'My Settings',
-    'Team Settings',
-    'Help & Feedback',
-    'Log Out',
-  ];
+  const isLoading = status === 'loading';
+
+  const collapseItems = ['Features', 'Customers', 'Pricing', 'Company', 'My Collections', 'Settings', 'Log Out'];
+
   return (
     <Navbar variant="floating">
+      <Navbar.Toggle aria-label="toggle navigation" showIn={'xs'} />
       <Navbar.Brand>
         <Text b color="inherit" hideIn="xs">
           Gearlist
@@ -30,16 +32,19 @@ export const Navigation = ({}) => {
         <Navbar.Link href="#">Pricing</Navbar.Link>
         <Navbar.Link href="#">Company</Navbar.Link>
       </Navbar.Content>
-      <Navbar.Content>
-        <Navbar.Link color="inherit" href="#">
+
+      {!session && (
+        <Navbar.Content>
+          {/* <Navbar.Link color="inherit" href="#">
           Login
-        </Navbar.Link>
-        <Navbar.Item>
-          <Button auto flat as={Link} href="#">
-            Sign Up
-          </Button>
-        </Navbar.Item>
-      </Navbar.Content>
+        </Navbar.Link> */}
+          <Navbar.Item>
+            <Button auto flat disabled={isLoading} as={Link} onClick={() => signIn()}>
+              {isLoading ? <Loading color="currentColor" size="sm" /> : 'Sign In'}
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      )}
       {session && (
         <>
           <Navbar.Content
@@ -58,36 +63,36 @@ export const Navigation = ({}) => {
                     as="button"
                     color="secondary"
                     size="md"
-                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                    src={session.user?.image || 'https://i.pravatar.cc/150?u=a042581f4e29026704d'}
                   />
                 </Dropdown.Trigger>
               </Navbar.Item>
               <Dropdown.Menu
                 aria-label="User menu actions"
                 color="secondary"
-                onAction={(actionKey) => console.log({ actionKey })}
+                onAction={(actionKey) => handleDropdownAction(actionKey)}
               >
                 <Dropdown.Item key="profile" css={{ height: '$18' }}>
+                  {/* <Text b color="inherit" css={{ d: 'flex' }}>
+                    Profile
+                  </Text> */}
                   <Text b color="inherit" css={{ d: 'flex' }}>
-                    Signed in as
-                  </Text>
-                  <Text b color="inherit" css={{ d: 'flex' }}>
-                    zoey@example.com
+                    {/* {session.user?.email || '...'} */}@{session.user.name || '...'}
                   </Text>
                 </Dropdown.Item>
+                <Dropdown.Item key="collections" withDivider>
+                  My Collections
+                </Dropdown.Item>
+                {/* <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item> */}
                 <Dropdown.Item key="settings" withDivider>
-                  My Settings
+                  Settings
                 </Dropdown.Item>
-                <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
-                <Dropdown.Item key="analytics" withDivider>
-                  Analytics
-                </Dropdown.Item>
-                <Dropdown.Item key="system">System</Dropdown.Item>
+                {/* <Dropdown.Item key="system">System</Dropdown.Item>
                 <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
                 <Dropdown.Item key="help_and_feedback" withDivider>
                   Help & Feedback
-                </Dropdown.Item>
-                <Dropdown.Item key="logout" withDivider color="error">
+                </Dropdown.Item> */}
+                <Dropdown.Item key="signOut" withDivider color="error">
                   Log Out
                 </Dropdown.Item>
               </Dropdown.Menu>
