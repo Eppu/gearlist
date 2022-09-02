ad54a207714b144972506f9ed5fa854cefff69b3
Eetu Eskelinen <eetuesk@gmail.com>
2022-09-03 00:19:51 +0300

    Add Manrope font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 import { Navigation } from '../components/Navigation';
+
+const theme = createTheme({
+  type: 'light',
+  theme: {
+    fonts: {
+      sans: 'Manrope, sans-serif',
+    },
+  },
+});
 
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -12,7 +21,9 @@ export default function App({ Component, pageProps }: AppProps) {
       // you have a short session maxAge time. Shown here with default values.
       session={pageProps.session}
     >
-      <NextUIProvider>
+      <NextUIProvider
+      // theme={theme}
+      >
         <Navigation />
         <Component {...pageProps} />
       </NextUIProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          {CssBaseline.flush()}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
