eec046a16d43148fcfd48a6708c92b6703aa8c31
Eetu Eskelinen <eetuesk@gmail.com>
2022-09-26 23:48:48 +0300

    Change hero components bg

--- components/HeroComponents.tsx
+++ components/HeroComponents.tsx
@@ -1,21 +1,5 @@
-import React, { useEffect } from 'react';
-import {
-  Input,
-  Card,
-  Row,
-  Col,
-  Loading,
-  Text,
-  styled,
-  Grid,
-  Pagination,
-  Tooltip,
-  Button,
-  StyledButton,
-  keyframes,
-  Container,
-  Image,
-} from '@nextui-org/react';
+import React from 'react';
+import { Card, Col, Text, styled, Grid, Container, Image } from '@nextui-org/react';
 
 import { levitating, appears } from '../utils/animations';
 
@@ -28,7 +12,13 @@ const StyledContainer = styled('div', {
 });
 
 export const Card1 = () => (
-  <Card css={{ animation: `${levitating} 15s ease infinite` }}>
+  <Card
+    css={{
+      animation: `${levitating} 15s ease infinite`,
+      boxShadow:
+        '0 50px 100px -20px rgba(50,50,93,.25),0 30px 60px -30px rgba(0,0,0,.3),inset 0 -2px 6px 0 rgba(10,37,64,.35);',
+    }}
+  >
     <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
       <Col>
         <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
@@ -50,7 +40,14 @@ export const Card1 = () => (
 );
 
 export const Card2 = () => (
-  <Card css={{ w: '100%', animation: `${levitating} 10s ease infinite` }}>
+  <Card
+    css={{
+      w: '100%',
+      animation: `${levitating} 10s ease infinite`,
+      boxShadow:
+        '0 50px 100px -20px rgba(50,50,93,.25),0 30px 60px -30px rgba(0,0,0,.3),inset 0 -2px 6px 0 rgba(10,37,64,.35);',
+    }}
+  >
     <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
       <Col>
         <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
@@ -70,29 +67,16 @@ export const Card2 = () => (
     />
   </Card>
 );
-// export const Card3 = () => (
-//   <Card css={{ bg: '$black', w: '100%', animation: `${levitating} 8s ease infinite` }}>
-//     <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-//       <Col>
-//         <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-//           Supercharged
-//         </Text>
-//         <Text h4 color="white">
-//           Creates beauty like a beast
-//         </Text>
-//       </Col>
-//     </Card.Header>
-//     <Card.Image
-//       src="https://nextui.org/images/card-example-2.jpeg"
-//       width="100%"
-//       height={340}
-//       objectFit="cover"
-//       alt="Card image background"
-//     />
-//   </Card>
-// );
 export const Card4 = () => (
-  <Card css={{ w: '100%', h: '400px', animation: `${levitating} 11s ease infinite` }}>
+  <Card
+    css={{
+      w: '100%',
+      h: '400px',
+      animation: `${levitating} 11s ease infinite`,
+      boxShadow:
+        '0 50px 100px -20px rgba(50,50,93,.25),0 30px 60px -30px rgba(0,0,0,.3),inset 0 -2px 6px 0 rgba(10,37,64,.35);',
+    }}
+  >
     <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
       <Col>
         <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
@@ -112,40 +96,18 @@ export const Card4 = () => (
         alt="Card example background"
       />
     </Card.Body>
-    {/* <Card.Footer
-      isBlurred
-      css={{
-        position: 'absolute',
-        bgBlur: '#ffffff66',
-        borderTop: '$borderWeights$light solid rgba(255, 255, 255, 0.2)',
-        bottom: 0,
-        zIndex: 1,
-      }}
-    >
-      <Row>
-        <Col>
-          <Text color="#000" size={12}>
-            Available soon.
-          </Text>
-          <Text color="#000" size={12}>
-            Get notified.
-          </Text>
-        </Col>
-        <Col>
-          <Row justify="flex-end">
-            <Button flat auto rounded color="secondary">
-              <Text css={{ color: 'inherit' }} size={12} weight="bold" transform="uppercase">
-                Notify Me
-              </Text>
-            </Button>
-          </Row>
-        </Col>
-      </Row>
-    </Card.Footer> */}
   </Card>
 );
 export const Card5 = () => (
-  <Card css={{ w: '100%', h: '400px', animation: `${levitating} 17s ease infinite` }}>
+  <Card
+    css={{
+      w: '100%',
+      h: '400px',
+      animation: `${levitating} 17s ease infinite`,
+      boxShadow:
+        '0 50px 100px -20px rgba(50,50,93,.25),0 30px 60px -30px rgba(0,0,0,.3),inset 0 -2px 6px 0 rgba(10,37,64,.35);',
+    }}
+  >
     <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
       <Col>
         <Text size={12} weight="bold" transform="uppercase" color="#9E9E9E">
@@ -165,56 +127,18 @@ export const Card5 = () => (
         alt="Relaxing app background"
       />
     </Card.Body>
-    {/* <Card.Footer
-      isBlurred
-      css={{
-        position: 'absolute',
-        bgBlur: '#0f111466',
-        borderTop: '$borderWeights$light solid $gray800',
-        bottom: 0,
-        zIndex: 1,
-      }}
-    >
-      <Row>
-        <Col>
-          <Row>
-            <Col span={3}>
-              <Card.Image
-                src="https://nextui.org/images/breathing-app-icon.jpeg"
-                css={{ bg: 'black', br: '50%' }}
-                height={40}
-                width={40}
-                alt="Breathing app icon"
-              />
-            </Col>
-            <Col>
-              <Text color="#d1d1d1" size={12}>
-                Breathing App
-              </Text>
-              <Text color="#d1d1d1" size={12}>
-                Get a good nights sleep.
-              </Text>
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row justify="flex-end">
-            <Button flat auto rounded css={{ color: '#94f9f0', bg: '#94f9f026' }}>
-              <Text css={{ color: 'inherit' }} size={12} weight="bold" transform="uppercase">
-                Get App
-              </Text>
-            </Button>
-          </Row>
-        </Col>
-      </Row>
-    </Card.Footer> */}
   </Card>
 );
 
 const HeroComponents = () => {
   return (
     <Container>
-      <Grid.Container gap={2} justify="center">
+      <Grid.Container
+        gap={2}
+        justify="center"
+        // Uncomment after BG blur asset is figured out
+        //  css={{ background: 'url("/images/bgblur.svg") no-repeat center;' }}
+      >
         <Grid xs={12} sm={7}>
           <Card1 />
         </Grid>
@@ -227,7 +151,7 @@ const HeroComponents = () => {
         <Grid xs={12} sm={8}>
           <Card5 />
         </Grid>
-        <Image
+        {/* <Image
           alt="gradient blue background"
           className="docs__gradient-blue"
           css={{
@@ -241,7 +165,7 @@ const HeroComponents = () => {
             animation: `${appears} 1000ms 500ms ease forwards`,
           }}
           src="/images/bgblur.svg"
-        />
+        /> */}
       </Grid.Container>
     </Container>
   );
