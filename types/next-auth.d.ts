45a07ea16e9e3bb89b57b414c99418e6c3247a69
Eetu Eskelinen <eetuesk@gmail.com>
2022-11-08 11:03:52 +0200

    Update next-auth type extensions

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,14 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 
-declare module "next-auth" {
+declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
+    userId: string;
     user: {
       /** The user's postal address. */
       id: number;
-    } & DefaultSession["user"];
+    } & DefaultSession['user'];
   }
 }
