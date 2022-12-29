70d34318812aa908f55499c7f194de6890d62d4d
Eetu Eskelinen <eetuesk@gmail.com>
2022-12-29 13:15:10 +0200

    Add item search to API and initial search functionality on new item page

---
+++ pages/api/items/search.ts
@@ -0,0 +1,53 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import checkSession from '../../../lib/checkSession';
+import { prisma } from '../../../lib/prisma';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const session = await checkSession(req, res);
+  // There's probably a smarter way to do this but I can't come up with one right now.
+  if (!session) {
+    res.status(401).json({ message: 'Invalid session' });
+    return;
+  }
+  // POST /api/items/search
+  if (req.method === 'POST') {
+    const { query, limit } = req.body;
+    // TODO: remove starting and trailing whitespace from query
+    const sanitizedQuery = query.trim();
+
+    // Find item templates that match the search term
+    await prisma.itemTemplate
+      .findMany({
+        take: limit || 10,
+        where: {
+          brand: {
+            search: sanitizedQuery.split(' ').join(' & '), // manual sanitization since Prisma doesn't support multiple string search terms
+          },
+          OR: [
+            {
+              model: {
+                search: sanitizedQuery.split(' ').join(' & ') as string, // manual sanitization
+              },
+            },
+          ],
+        },
+      })
+      .then((items) => {
+        res.status(200).json({ count: items.length, items });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'Items retrieval failed',
+          error: err,
+        });
+      });
+    // }
+    // res.status(400).json({ message: 'No search term provided' });
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
---
+++ pages/item/new.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Container, Row, Input, Spacer, Loading } from '@nextui-org/react';
+import { Layout } from '../../components/Layout';
+import debounce from 'lodash.debounce';
+
+type Inputs = {
+  //   example: string;
+  brand: string;
+  model: string;
+};
+
+type SearchResult = {
+  brand: string;
+  model: string;
+  id: string;
+  category: string;
+  createdAt: string;
+  updatedAt: string;
+  imageUrl: string;
+};
+
+export default function NewItem() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  //   console.log(watch('brand')); // watch input value by passing the name of it
+
+  const [brand, setBrand] = useState('');
+  const [model, setModel] = useState('');
+  const [selectedTemplate, setSelectedTemplate] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
+  const searchBrand = async (brand: string) => {
+    setIsLoading(true);
+    const res = await fetch(`/api/items/search`, {
+      method: 'POST',
+      body: JSON.stringify({ query: brand }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    console.log(`got data with query ${brand}`, data);
+    setIsLoading(false);
+    setSearchResults(data.items);
+
+    return data;
+  };
+
+  const debouncedSearchBrand = debounce(searchBrand, 500);
+
+  return (
+    <Layout>
+      <Container css={{ mt: '$20' }}>
+        {isLoading && <Loading />}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <Input
+            clearable
+            bordered
+            labelPlaceholder="Search for an item"
+            // value={brand}
+            onChange={(e) => debouncedSearchBrand(e.target.value)}
+          />
+          <Spacer y={1} />
+          {/*  if there are search results, display them */}
+          {searchResults.length > 0 && (
+            <Container css={{ p: '$0' }}>
+              {searchResults.map((result) => (
+                <Row
+                  key={result.id}
+                  onClick={(e) => setSelectedTemplate(result.id)}
+                >
+                  <a>
+                    {result.brand} {result.model}
+                  </a>
+                  <Spacer y={2} />
+                </Row>
+              ))}
+            </Container>
+          )}
+
+          <input type="submit" />
+        </form>
+      </Container>
+    </Layout>
+  );
+}
