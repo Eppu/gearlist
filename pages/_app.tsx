6cc991b0e02604194da1eca8a45395b0a977e0ad
Eetu Ekslinen <eetuesk@gmail.com>
2023-09-11 11:27:59 +0300

    Install NextUI v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { Navigation } from '../components/Navigation';
 import { Session } from 'next-auth';
@@ -8,24 +8,25 @@ import { useSession } from 'next-auth/react';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import ProgressBar from '../components/ProgressBar';
+import '../styles/globals.css';
 
-const lightTheme = createTheme({
-  type: 'light',
-  theme: {
-    fonts: {
-      sans: 'Manrope, sans-serif',
-    },
-  },
-});
+// const lightTheme = createTheme({
+//   type: 'light',
+//   theme: {
+//     fonts: {
+//       sans: 'Manrope, sans-serif',
+//     },
+//   },
+// });
 
-const darkTheme = createTheme({
-  type: 'dark',
-  theme: {
-    fonts: {
-      sans: 'Manrope, sans-serif',
-    },
-  },
-});
+// const darkTheme = createTheme({
+//   type: 'dark',
+//   theme: {
+//     fonts: {
+//       sans: 'Manrope, sans-serif',
+//     },
+//   },
+// });
 
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -39,10 +40,10 @@ export default function App({ Component, pageProps }: AppProps<{ session: Sessio
       <NextThemesProvider
         defaultTheme="system"
         attribute="class"
-        value={{
-          light: lightTheme.className,
-          dark: darkTheme.className,
-        }}
+        // value={{
+        //   light: lightTheme.className,
+        //   dark: darkTheme.className,
+        // }}
       >
         <ProgressBar />
         <NextUIProvider>
